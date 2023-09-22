5f1362cef9de75fe895398d99bf27aee593c39ce
tuo.wang <wangtuo1@100tal.com>
2023-09-22 16:07:45 +0800

    新建测试文件

---
+++ test/test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time : 2023/9/21 10:12 上午
+# @Author : tuo.wang
+# @Version : 
+# @Function :
+
+#
+# result = {
+#     'cloud_paper': 0,
+#     'peiyou_paper': 0,
+#     'personal_paper': 0,
+#     'tipaipai_paper': 0,
+# }
+#
+# with open('/Users/tuo/Downloads/test_data_filter.txt', 'r') as f:
+#     for line in f.readlines():
+#         file_path = line.strip().split(':')[-2].split(',')[0].split('/')[-2]
+#         file_data_len = int(line.strip().split(':')[-1])
+#         result[file_path] += file_data_len
+#
+# print(result)
+print(2336879-2331408)
